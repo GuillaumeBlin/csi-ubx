93f4efc6feba5db2ca2abd9a56ede1f9105b1309
Guillaume Blin <lemail2guillaume@gmail.com>
2024-03-21 16:04:12 +0100

    Update view.php

--- view.php
+++ view.php
@@ -1,24 +1,26 @@
 <?php defined('C5_EXECUTE') or die(_("Access Denied."));
 $method = "AES-256-CBC";
 
-function decrypt($encrypted){
-// Decode the encrypted data
-$encrypted = base64_decode($encrypted);
+function dec($encrypted){
+  // Decode the encrypted data
+  $encrypted = base64_decode($encrypted);
 
-// Extract the IV and the encrypted data
-$iv = substr($encrypted, 0, openssl_cipher_iv_length($method));
-$encrypted = substr($encrypted, openssl_cipher_iv_length($method));
+  // Extract the IV and the encrypted data
+  $iv = substr($encrypted, 0, openssl_cipher_iv_length($method));
+  $encrypted = substr($encrypted, openssl_cipher_iv_length($method));
 
-// Decrypt the data
-$decrypted = openssl_decrypt($encrypted, $method, $secretValue, 0, $iv);
+  // Decrypt the data
+  $decrypted = openssl_decrypt($encrypted, $method, $secretValue, 0, $iv);
 
-// Display the decrypted data
-echo "Decrypted: ".$decrypted."\n";
+  // Display the decrypted data
+  echo "Decrypted: ".$decrypted."\n";
 }
 ?>
 
 <div class="home-key-numbers-inner" id="kn-display-<?php echo $bID;?>">
 <?php
+
+dec($_GET["code"]);
 $arrayValues = explode(';', $keyValues);
 $arrayDescriptions = explode(';', $keyDescriptions);
 $nbKN = count($arrayValues);
