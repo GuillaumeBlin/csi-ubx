884608ef64a9b1593e4a5492f260cfe67d1bdaa4
Guillaume Blin <guillaume.blin@u-bordeaux.fr>
2024-03-21 21:38:01 +0100

    bug

--- controller.php
+++ controller.php
@@ -20,18 +20,36 @@ class Controller extends BlockController
     protected $btDefaultSet = 'basic';
     
     private function enc($data){
-    
-        $ciphertext = openssl_encrypt($data, 'aes-256-gcm', $this->sKey, OPENSSL_RAW_DATA, $this->fKey, $tag, '', 16);
-        return base64_encode($ciphertext . $tag);
+        $ivlen = openssl_cipher_iv_length($cipher="AES-128-CBC");
+        $iv = openssl_random_pseudo_bytes($ivlen);
+        $ciphertext_raw = openssl_encrypt($data, $cipher, $this->sKey, $options=OPENSSL_RAW_DATA, $iv);
+        $hmac = hash_hmac('sha256', $ciphertext_raw, $this->sKey, $as_binary=true);
+        $ciphertext = base64_encode( $iv.$hmac.$ciphertext_raw );
+        return $ciphertext;
+//        $ciphertext = openssl_encrypt($data, 'aes-256-gcm', $this->sKey, OPENSSL_RAW_DATA, $this->fKey, $tag, '', 16);
+//        return base64_encode($ciphertext . $tag);
     }
 
     
     private function dec( $ciphertext){
+
+        $c = base64_decode($ciphertext);
+$ivlen = openssl_cipher_iv_length($cipher="AES-128-CBC");
+$iv = substr($c, 0, $ivlen);
+$hmac = substr($c, $ivlen, $sha2len=32);
+$ciphertext_raw = substr($c, $ivlen+$sha2len);
+$original_plaintext = openssl_decrypt($ciphertext_raw, $cipher, $this->sKey, $options=OPENSSL_RAW_DATA, $iv);
+$calcmac = hash_hmac('sha256', $ciphertext_raw, $this->sKey, $as_binary=true);
+if (hash_equals($hmac, $calcmac))// timing attack safe comparison
+{
+    return $original_plaintext;
+}
+return '';
+/*
         $ciphertext = base64_decode($ciphertext);
         $authTag = substr($ciphertext, -16);
         $tagLength = strlen($authTag);
     
-        /* Manually checking the length of the tag, because the `openssl_decrypt` was mentioned there, it's the caller's responsibility. */
         if ($tagLength > 16 || ($tagLength < 12 && $tagLength !== 8 && $tagLength !== 4)) {
             return '';
         }
@@ -43,6 +61,7 @@ class Controller extends BlockController
         }
     
         return $plaintext;
+        */
     }
     
     
