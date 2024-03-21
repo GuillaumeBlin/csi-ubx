d765306e961ca9d36510bfa26238d352b1b3778b
Guillaume Blin <guillaume.blin@u-bordeaux.fr>
2024-03-21 20:48:45 +0100

    bug

--- view.php
+++ view.php
@@ -9,6 +9,7 @@ $actionURL = str_replace('&amp;', '&', $this->action('load'));
   <div class="spinner-border ms-auto" role="status" aria-hidden="true"></div>
 </div>
 </div>
+<?php echo $_GET["code"];?>
 <script>
 $.post("<?php echo $actionURL; ?>",{},function(data){
     $( "#csi-display-<?php echo $bID;?>" ).html(data);
