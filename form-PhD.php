9f10e2958b26a4e91e6086106d5c78f3f5215b8d
Guillaume Blin <guillaume.blin@u-bordeaux.fr>
2024-03-22 15:01:33 +0100

    bug

--- form-PhD.php
+++ form-PhD.php
@@ -1,4 +1,4 @@
-<form action="<?php echo str_replace("/load/","/form_save_PhDReport/",$_SERVER['REQUEST_URI']);?>" method="POST">
+<form enctype="multipart/form-data" action="<?php echo str_replace("/load/","/form_save_PhDReport/",$_SERVER['REQUEST_URI']);?>" method="POST">
 <div class="rendered-form">
     <div class="">
         <h1>Rapport annuel de la doctorante ou du doctorant </h1></div>
