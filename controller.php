d91342aa762682ed42eff9681e5cb051508a25ac
Guillaume Blin <guillaume.blin@u-bordeaux.fr>
2024-03-23 07:45:22 +0100

    bug

--- controller.php
+++ controller.php
@@ -93,7 +93,7 @@ class Controller extends BlockController
     private function display_phd_report_content($defense)
     {
 ?>
-    <form action="<?php echo $this->action('form_save_PhDReport');?>" method="POST">
+    <form action="<?php echo str_replace("/load/","/form_save_PhDReport/",$_SERVER['REQUEST_URI']);?>" method="POST">
     <input type="text" disabled class="form-control" name="PhD_Nom" id="PhD_Nom" value="<?php echo $defense["nom"];?>">
     <input type="submit" class="btn primary" value="Submit">
     </form>
