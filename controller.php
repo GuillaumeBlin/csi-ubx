18f06cefb6b9892446c177c97060b2a2a0f1a6f1
Guillaume Blin <guillaume.blin@u-bordeaux.fr>
2024-03-24 08:05:42 +0100

    bug

--- controller.php
+++ controller.php
@@ -82,8 +82,8 @@ class Controller extends BlockController
     private function display_links($defense)
     {
         echo "<li>".$this->totitle($defense["prenom"]) . ' ' . $defense["nom"];
-            echo "<ul><li>".urlencode($this->enc("csi-".$defense["Matricule_etudiant"]."-PhD"));
-            echo " -&gt; ".$defense["mail_principal"]. " (".$defense["mail_secondaire"].")".$defense["Matricule_etudiant"]."</li>";
+            echo "<ul><li>".htmlspecialchars(urlencode($this->enc("csi-".$defense["Matricule_etudiant"]."-PhD")));
+            echo " -&gt; ".$defense["mail_principal"]. " (".$defense["mail_secondaire"].")</li>";
             echo "<li>".htmlspecialchars(urlencode($this->enc("csi-".$defense["Matricule_etudiant"]."-DT"))); 
             echo " -&gt; ".$defense["these_directeur_these_mail"]."</li>";
             echo "<li>".htmlspecialchars(urlencode($this->enc("csi-".$defense["Matricule_etudiant"]."-CSI"))); 
