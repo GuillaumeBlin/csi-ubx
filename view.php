ac9970d0e8889da31bdf4b0ce0a46a8b9505bc09
Guillaume Blin <guillaume.blin@u-bordeaux.fr>
2024-03-26 17:33:07 +0100

    bug

--- view.php
+++ view.php
@@ -11,7 +11,9 @@ if($admin =='True'){
 
 <link rel="stylesheet" href="https://cdn.datatables.net/2.0.3/css/dataTables.dataTables.css" />  
 <script src="https://cdn.datatables.net/2.0.3/js/dataTables.js"></script>
-<h1 onclick="$('#admin-links-display-<?php echo $bID;?>').toggle();"> Liens de connection</h1>
+<h1 onclick="$('#admin-links-display-<?php echo $bID;?>').toggle();"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-right-square-fill" viewBox="0 0 16 16">
+  <path d="M14 16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2zM5.904 5.197 10 9.293V6.525a.5.5 0 0 1 1 0V10.5a.5.5 0 0 1-.5.5H6.525a.5.5 0 0 1 0-1h2.768L5.197 5.904a.5.5 0 0 1 .707-.707"/>
+</svg> Liens de connection</h1>
 
 <div id="admin-links-display-<?php echo $bID;?>" style="display:none">
   <div class="d-flex align-items-center">
