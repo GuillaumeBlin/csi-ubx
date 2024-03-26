07ee43ffd26797ab70750cf0fc893c835250cd89
Guillaume Blin <guillaume.blin@u-bordeaux.fr>
2024-03-26 17:34:22 +0100

    bug

--- view.php
+++ view.php
@@ -24,8 +24,8 @@ if($admin =='True'){
 <h1 onclick="$('#admin-PhD-display-<?php echo $bID;?>').toggle();"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-right-square-fill" viewBox="0 0 16 16">
   <path d="M14 16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2zM5.904 5.197 10 9.293V6.525a.5.5 0 0 1 1 0V10.5a.5.5 0 0 1-.5.5H6.525a.5.5 0 0 1 0-1h2.768L5.197 5.904a.5.5 0 0 1 .707-.707"/>
 </svg> Rapport des doctorantes et doctorantes</h1>
-<div id="admin-PhD-display-<?php echo $bID;?>">
-  <div class="d-flex align-items-center">
+<div id="admin-PhD-display-<?php echo $bID;?>" style="display:none">
+  <div class="d-flex align-items-center" >
     <strong>Loading...</strong>
     <div class="spinner-border ms-auto" role="status" aria-hidden="true"></div>
   </div>
@@ -33,7 +33,7 @@ if($admin =='True'){
 <h1 onclick="$('#admin-DT-display-<?php echo $bID;?>').toggle();"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-right-square-fill" viewBox="0 0 16 16">
   <path d="M14 16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2zM5.904 5.197 10 9.293V6.525a.5.5 0 0 1 1 0V10.5a.5.5 0 0 1-.5.5H6.525a.5.5 0 0 1 0-1h2.768L5.197 5.904a.5.5 0 0 1 .707-.707"/>
 </svg> Rapport des directions de thèse</h1>
-<div id="admin-DT-display-<?php echo $bID;?>">
+<div id="admin-DT-display-<?php echo $bID;?>" style="display:none">
   <div class="d-flex align-items-center">
     <strong>Loading...</strong>
     <div class="spinner-border ms-auto" role="status" aria-hidden="true"></div>
@@ -42,7 +42,7 @@ if($admin =='True'){
 <h1 onclick="$('#admin-CSI-display-<?php echo $bID;?>').toggle();"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-right-square-fill" viewBox="0 0 16 16">
   <path d="M14 16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2zM5.904 5.197 10 9.293V6.525a.5.5 0 0 1 1 0V10.5a.5.5 0 0 1-.5.5H6.525a.5.5 0 0 1 0-1h2.768L5.197 5.904a.5.5 0 0 1 .707-.707"/>
 </svg> Rapport des comités de CSI</h1>
-<div id="admin-CSI-display-<?php echo $bID;?>">
+<div id="admin-CSI-display-<?php echo $bID;?>" style="display:none">
   <div class="d-flex align-items-center">
     <strong>Loading...</strong>
     <div class="spinner-border ms-auto" role="status" aria-hidden="true"></div>
