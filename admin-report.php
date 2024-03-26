50c3a0a2f1b9a348957ca18baad9bb71a591c705
Guillaume Blin <guillaume.blin@u-bordeaux.fr>
2024-03-26 16:57:01 +0100

    bug

--- admin-report.php
+++ admin-report.php
@@ -1,6 +1,4 @@
-<link rel="stylesheet" href="https://cdn.datatables.net/2.0.3/css/dataTables.dataTables.css" />
-  
-<script src="https://cdn.datatables.net/2.0.3/js/dataTables.js"></script>
+
 <table id="report" class="display">
     <thead>
         <tr>
@@ -30,73 +28,68 @@
 </table>
 
 <script type="text/javascript">
-        $( document ).ready(function() {
-            function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+$( document ).ready(function() {
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
 
-            sleep(3000).then(() => { 
+    function format(d) {
+        return (
+    <?php        
+            $i=1;
+            foreach ($report_headers as $row) {
+                echo "'".$row["COLUMN_NAME"]." :'+d[".$i."]+'<br>'+\n";                
+                $i=$i+1;
+            }
+            echo "'<br>'"
+    ?>
+        );
+    }
 
-function format(d) {
-    console.log(d);
-    return (
-        <?php        
-                $i=1;
-                foreach ($report_headers as $row) {
-                    echo "'".$row["COLUMN_NAME"]." :'+d[".$i."]+'<br>'+\n";                
-                    $i=$i+1;
-                }
-                echo "'<br>'"
-            ?>
-
+    sleep(2000).then(() => { 
+        const table = new DataTable('#report', {
+            columnDefs: [
+                { targets: [0, 2,3,4,43], visible: true},
+                { targets: '_all', visible: false }
+            ]
+        });
+ 
+        // Array to track the ids of the details displayed rows
+        const detailRows = [];
         
-    );
-}
- 
-const table = new DataTable('#report', {
-    columnDefs: [
-        { targets: [0, 2,3,4,43], visible: true},
-        { targets: '_all', visible: false }
-    ]
-});
- 
-// Array to track the ids of the details displayed rows
-const detailRows = [];
- 
-table.on('click', 'tbody td.dt-control', function () {
-    let tr = event.target.closest('tr');
-    let row = table.row(tr);
-    let idx = detailRows.indexOf(tr.id);
- 
-    if (row.child.isShown()) {
-        tr.classList.remove('details');
-        row.child.hide();
- 
-        // Remove from the 'open' array
-        detailRows.splice(idx, 1);
-    }
-    else {
-        tr.classList.add('details');
-        row.child(format(row.data())).show();
- 
-        // Add to the 'open' array
-        if (idx === -1) {
-            detailRows.push(tr.id);
-        }
-    }
-});
- 
-// On each draw, loop over the `detailRows` array and show any child rows
-table.on('draw', () => {
-    detailRows.forEach((id, i) => {
-        let el = document.querySelector('#' + id + ' td.dt-control');
- 
-        if (el) {
-            el.dispatchEvent(new Event('click', { bubbles: true }));
-        }
+        table.on('click', 'tbody td.dt-control', function () {
+            let tr = event.target.closest('tr');
+            let row = table.row(tr);
+            let idx = detailRows.indexOf(tr.id);
+        
+            if (row.child.isShown()) {
+                tr.classList.remove('details');
+                row.child.hide();
+        
+                // Remove from the 'open' array
+                detailRows.splice(idx, 1);
+            }
+            else {
+                tr.classList.add('details');
+                row.child(format(row.data())).show();
+        
+                // Add to the 'open' array
+                if (idx === -1) {
+                    detailRows.push(tr.id);
+                }
+            }
+        });
+        
+        // On each draw, loop over the `detailRows` array and show any child rows
+        table.on('draw', () => {
+            detailRows.forEach((id, i) => {
+                let el = document.querySelector('#' + id + ' td.dt-control');
+        
+                if (el) {
+                    el.dispatchEvent(new Event('click', { bubbles: true }));
+                }
+            });
+        });
     });
 });
-
-   });
-        });
 </script>
--- view.php
+++ view.php
@@ -3,11 +3,14 @@
 
 $actionURL = str_replace('&amp;', '&', $this->action('load'))."?code=".$_GET["code"];
 ?>
+<link rel="stylesheet" href="https://cdn.datatables.net/2.0.3/css/dataTables.dataTables.css" />  
+<script src="https://cdn.datatables.net/2.0.3/js/dataTables.js"></script>
+<h1><?php echo $admin;?></h1>
 <div id="csi-display-<?php echo $bID;?>">
-<div class="d-flex align-items-center">
-  <strong>Loading...</strong>
-  <div class="spinner-border ms-auto" role="status" aria-hidden="true"></div>
-</div>
+  <div class="d-flex align-items-center">
+    <strong>Loading...</strong>
+    <div class="spinner-border ms-auto" role="status" aria-hidden="true"></div>
+  </div>
 </div>
 <script>
 $.post("<?php echo $actionURL; ?>",{},function(data){
