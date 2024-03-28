74fe119e8c0e64697b495670bdcf33e37cfe9669
Guillaume Blin <guillaume.blin@u-bordeaux.fr>
2024-03-28 11:11:52 +0100

    bug

--- controller.php
+++ controller.php
@@ -192,7 +192,7 @@ class Controller extends BlockController
             echo "Il est visible ici : <i class='far fa-file-alt'></i>";
             echo "<script>$('.fa-file-alt').on('click',function(e){";
                 //echo "    if(e.target.getAttribute('token')) {";
-                echo '      $.post("'.str_replace("/load_admin_links/","/show_PhDReport/",$_SERVER['REQUEST_URI']).'",{},function(data){';
+                echo '      $.post("'.str_replace("/load_user/","/show_PhDReport/",$_SERVER['REQUEST_URI']).'",{},function(data){';
                 echo "        console.log(data);";
                 echo "      });";
                 //echo "    }";
