97c345ec289d0eb55bc90967a878179636698e0d
Guillaume Blin <guillaume.blin@u-bordeaux.fr>
2024-03-29 14:57:02 +0100

    bug

--- admin-report.php
+++ admin-report.php
@@ -78,6 +78,7 @@ $( document ).ready(function() {
                 console.log(data);
               //  table.row('.selected').remove().draw(false);
             });
+            table.draw();
         });
         });                
 
