763f06a3ecdb7be2e3bb338fe115c00b86999d2d
Guillaume Blin <guillaume.blin@u-bordeaux.fr>
2024-03-29 14:54:01 +0100

    bug

--- admin-report.php
+++ admin-report.php
@@ -72,7 +72,7 @@ $( document ).ready(function() {
         $("#report_button").on("click",function(){
             table.rows({selected: true}).each(function(){
                 console.log(this.data()[0]);
-                var anId=intval(this.data()[0][2]);
+                var anId=this.data()[0][2];
                 console.log(anId);
                 $.post("<?php echo $actionRemovePhDReport; ?>",{id: anId},function(data){
                 console.log(data);
