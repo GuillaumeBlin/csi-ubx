802891966a86cf75425790a1a102be054a2b6ff9
Guillaume Blin <guillaume.blin@u-bordeaux.fr>
2024-03-29 14:13:05 +0100

    bug

--- form-PhD.php
+++ form-PhD.php
@@ -1,30 +1,30 @@
 <form action="<?php echo str_replace("/load_user/","/form_save_PhDReport/",$_SERVER['REQUEST_URI']);?>" method="POST">
 
     <h1>Rapport annuel de la doctorante ou du doctorant </h1>
-    <h2>INFORMATION GÉNÉRALES</h2>
-    <h3>LA DOCTORANTE OU LE DOCTORANT</h3>
-                <h4>Nom</h4>
+    <h3>Informations générales</h3>
+    <h4>La doctorante ou le doctorant</h4>
+                <h5>Nom</h5>
                 <input type="text" readonly class="form-control" name="PhD_Nom" id="PhD_Nom" value="<?php echo $defense["nom"];?>">
-                <h4>Prénom</h4>
+                <h5>Prénom</h5>
                 <input type="text" readonly class="form-control" name="PhD_Prenom" id="PhD_Prenom" value="<?php echo $defense["prenom"];?>">
-                <h4>Email dans ADUM</h4>
+                <h5>Email dans ADUM</h5>
                 <input type="text" readonly class="form-control" name="PhD_Mail" id="PhD_Mail" value="<?php echo $defense["mail_principal"];?>">
-                <h4>Spécialité</h4>
+                <h5>Spécialité</h5>
                 <input type="text" readonly class="form-control" name="PhD_Specialite" id="PhD_Specialite" value="<?php echo $defense["these_specialite"];?>">
-                <h4>Unité de recherche</h4>
+                <h5>Unité de recherche</h5>
                 <input type="text" readonly class="form-control" name="PhD_UMR" id="PhD_UMR" value="<?php echo $defense["these_laboratoire"];?>">
-    <h3>LA THÈSE</h3>
-                <h4>Nom direction de thèse</h4>
+    <h4>La thèse</h4>
+                <h5>Nom direction de thèse</h5>
                 <input type="text" readonly class="form-control" name="DT_Nom" id="DT_Nom" value="<?php echo $defense["these_directeur_these_nom"];?>">
-                <h4>Prénom direction de thèse</h4>
+                <h5>Prénom direction de thèse</h5>
                 <input type="text" readonly class="form-control" name="DT_Prenom" id="DT_Prenom" value="<?php echo $defense["these_directeur_these_prenom"];?>">
-                <h4>Nom co-direction de thèse</h4>
+                <h5>Nom co-direction de thèse</h5>
                 <input type="text" readonly class="form-control" name="CODT_Nom" id="CODT_Nom" value="<?php echo $defense["these_codirecteur_these_nom"];?>">
-                <h4>Prénom co-direction de thèse</h4>
+                <h5>Prénom co-direction de thèse</h5>
                 <input type="text" readonly class="form-control" name="CODT_Prenom" id="CODT_Prenom" value="<?php echo $defense["these_codirecteur_these_prenom"];?>">
-        <h4>Date de début de thèse</h4>
+        <h5>Date de début de thèse</h5>
         <input type="date" class="form-control" name="PhD_DateDebutThese" readonly  id="PhD_DateDebutThese" value="<?php echo date('Y-m-d', strtotime($defense["these_date_1inscription"])); ?>">
-        <h4>Type de financement</h4>
+        <h5>Type de financement</h5>
             <span>
                 <input name="TypeDeFinancement" value="Ministère" type="radio">
                 <label>Ministères</label>
@@ -47,7 +47,7 @@
                 </label>
                 <input type="text" id="TypeDeFinancementAutre" class="other-val">
             </div>
-        <h4>Thèse en cotutelle</h4>        
+        <h5>Thèse en cotutelle</h5>        
             <span>
                 <input name="PhD_Cotutelle" value="Oui" type="radio" readonly <?php if($defense["these_cotutelle"]=="OUI") {echo "checked";}else{echo "disabled";};?>>
                 <label>Oui</label>
@@ -64,7 +64,7 @@
     <?php 
             }
     ?>
-        <h4>Thèse avec activités complémentaires (enseignement, médiation, valorisation expertise)</h4>
+        <h5>Thèse avec activités complémentaires (enseignement, médiation, valorisation expertise)</h5>
             <span>
                 <input name="PhD_ExtraActivite" value="Oui" type="radio">
                 <label>Oui</label>
@@ -77,22 +77,24 @@
         <label for="PhD_ExtraActivite_NbH" >Si oui, nombre d’heures :</label>
         <input type="number" class="form-control" name="PhD_ExtraActivite_NbH" min="0" max="64" step="1" id="PhD_ExtraActivite_NbH">
     </div>
-    <h3>ANNÉE DU CSI</h3>
-        <h4>CSI pour réinscription en année</h4>                
+
+    <h4>Année du CSI</h4>
+        <h5>CSI pour réinscription en année</h5>                
         <input type="number" readonly class="form-control" name="PhD_CSI_Annee" min="2" max="8" step="1" id="PhD_CSI_Annee" value="<?php echo intval(substr($defense["niveau_Etud"],0,1))+1;?>">
-    <h3>COMPOSITION DU COMITÉ DE SUIVI INDIVIDUEL (hors direction de thèse)</h3>
-            <h4>Membre n°1</h4>
+    <h4>Composition du comité de suivi individuel</h4>
+            <h5>Membre n°1</h5>
                 <?php //print_r($defense["csi"]);
                 ?> 
-                <h5>Nom</h5>
+                <h6>Nom</h6>
                 <input type="text" class="form-control" name="CSI_Membre_1_Nom" id="CSI_Membre_1_Nom" readonly value="<?php echo $defense["csi"][0]["nom"];?>">
-            <h5>Prénom</h5>
+            <h6>Prénom</h6>
                 <input type="text" class="form-control" name="CSI_Membre_1_Prenom" id="CSI_Membre_1_Prenom" readonly value="<?php echo $defense["csi"][0]["prenom"];?>">
-            <h5>Unité de recherche/établissement</h5>
+            <h6>Unité de recherche/établissement</h6>
                 <input type="text" class="form-control" name="CSI_Membre_1_UMR" id="CSI_Membre_1_UMR" >
-            <h5>Adresse mail</h5>
-                <input type="email" class="form-control" name="CSI_Membre_1_mail" id="CSI_Membre_1_mail" value="<?php echo $defense["csi"][0]["mail"];?>">
-            <div>
+            <h6>Adresse mail</h6>
+                <input type="email" class="form-control" name="CSI_Membre_1_mail" id="CSI_Membre_1_mail" readonly value="<?php echo $defense["csi"][0]["mail"];?>">
+                <h6>Qualité</h6>
+                <div>
                 <input name="CSI_Membre_1_qualite" value="Spécialiste du domaine de la thèse" type="radio" <?php if($defense["csi"][0]["membre_specialiste"]=="oui"){echo "checked";}?>>
                 <label>Spécialiste du domaine de la thèse</label>
             </div>
@@ -104,16 +106,17 @@
                 <input name="CSI_Membre_1_qualite" value="Membre extérieur à l'établissement" type="radio" <?php if($defense["csi"][0]["membre_exterieur"]=="oui"){echo "checked";}?>>
                 <label>Membre extérieur à l'établissement</label>
             </div>
-            <h4>Membre n°2</h4>
-            <h5>Nom</h5>
+            <h5>Membre n°2</h5>
+            <h6>Nom</h6>
                 <input type="text" class="form-control" name="CSI_Membre_2_Nom" id="CSI_Membre_2_Nom" readonly value="<?php echo $defense["csi"][1]["nom"];?>">
-            <h5>Prénom</h5>
+            <h6>Prénom</h6>
                 <input type="text" class="form-control" name="CSI_Membre_2_Prenom" id="CSI_Membre_2_Prenom" readonly value="<?php echo $defense["csi"][1]["prenom"];?>">
-            <h5>Unité de recherche/établissement</h5>
+            <h6>Unité de recherche/établissement</h6>
                 <input type="text" class="form-control" name="CSI_Membre_2_UMR" id="CSI_Membre_2_UMR">
-            <h5>Adresse mail</h5>
-                <input type="email" class="form-control" name="CSI_Membre_2_mail" id="CSI_Membre_2_mail" value="<?php echo $defense["csi"][1]["mail"];?>">
-            <div>
+            <h6>Adresse mail</h6>
+                <input type="email" class="form-control" name="CSI_Membre_2_mail" id="CSI_Membre_2_mail" readonly value="<?php echo $defense["csi"][1]["mail"];?>">
+                <h6>Qualité</h6>
+                <div>
                 <input name="CSI_Membre_2_qualite" value="Spécialiste du domaine de la thèse" type="radio" <?php if($defense["csi"][1]["membre_specialiste"]=="oui"){echo "checked";}?>>
                 <label>Spécialiste du domaine de la thèse</label>
             </div>
@@ -125,16 +128,17 @@
                 <input name="CSI_Membre_2_qualite" value="Membre extérieur à l'établissement" type="radio" <?php if($defense["csi"][1]["membre_exterieur"]=="oui"){echo "checked";}?>>
                 <label>Membre extérieur à l'établissement</label>
             </div>        
-            <h4>Membre n°3</h4>
-            <h5>Nom</h5>
+            <h5>Membre n°3</h5>
+            <h6>Nom</h6>
                 <input type="text" class="form-control" name="CSI_Membre_3_Nom" id="CSI_Membre_3_Nom" readonly value="<?php echo $defense["csi"][2]["nom"];?>">
-                <h5>Prénom</h5>
+                <h6>Prénom</h6>
                 <input type="text" class="form-control" name="CSI_Membre_3_Prenom" id="CSI_Membre_3_Prenom" readonly value="<?php echo $defense["csi"][2]["nom"];?>">
-                <h5>Unité de recherche/établissement</h5>
+                <h6>Unité de recherche/établissement</h6>
                 <input type="text" class="form-control" name="CSI_Membre_3_UMR" id="CSI_Membre_3_UMR">
-                <h5>Adresse mail</h5>
-                <input type="email" class="form-control" name="CSI_Membre_3_mail" id="CSI_Membre_3_mail" value="<?php echo $defense["csi"][2]["mail"];?>">
-            <div>
+                <h6>Adresse mail</h6>
+                <input type="email" class="form-control" name="CSI_Membre_3_mail" id="CSI_Membre_3_mail" readonly value="<?php echo $defense["csi"][2]["mail"];?>">
+                <h6>Qualité</h6>
+                <div>
                 <input name="CSI_Membre_3_qualite" value="Spécialiste du domaine de la thèse" type="radio" <?php if($defense["csi"][2]["membre_specialiste"]=="oui"){echo "checked";}?>>
                 <label>Spécialiste du domaine de la thèse</label>
             </div>
@@ -146,7 +150,7 @@
                 <input name="CSI_Membre_3_qualite" value="Membre extérieur à l'établissement" type="radio" <?php if($defense["csi"][2]["membre_exterieur"]=="oui"){echo "checked";}?>>
                 <label>Membre extérieur à l'établissement</label>
             </div>
-    <h3>1 -&nbsp; BILAN ANNUEL AVEC LA DIRECTION DE THÈSE</h3>
+    <h3>Bilan annuel avec la direction de thèse</h3>
         <h4>Fréquence des contacts avec la direction de thèse (en dehors des courriers électroniques)</h4>
             <span>
                 <input name="Freq_Contact_DT" value="Tous les jours" type="radio">
@@ -189,7 +193,7 @@
                 <input name="Freq_Contact_CODT" value="Moins d'une fois par mois" type="radio">
                 <label>Moins d'une fois par mois</label>
             </span>
-    <h3>2- BILAN ANNUEL DE LA RELATION AVEC L’UNITÉ DE RECHERCHE</h3>
+    <h3>Bilan annuel de la relation avec l'unité de recherche</h3>
         <h4>Intégration dans l’unité de recherche)</h4>
             <span>
                 <input name="Integration_UMR" value="Peu satisfaisante" type="radio">
@@ -216,7 +220,7 @@
         <label for="Relation_Autre_EP_Details" >Si oui, préciser lesquelles et si elles sont nationales ou internationales :</label>
         <textarea type="textarea" class="form-control" name="Relation_Autre_EP_Details" id="Relation_Autre_EP_Details"></textarea>
     </div>
-    <h3>3 -&nbsp;AVIS GÉNÉRAL SUR L’ANNÉE ÉCOULÉE</h3>
+    <h3>Avis général sur l'année écoulée</h3>
         <p>Donner votre avis personnel sur les points suivants&nbsp;</p>
         <h4>Avez-vous assez d'autonomie pour gérer votre travail ?&nbsp;</h4>
             <span>
