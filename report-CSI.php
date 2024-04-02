1c04591bf68eb022b71c3efd3565daa6ad1f16ed
Guillaume Blin <guillaume.blin@u-bordeaux.fr>
2024-04-02 16:08:03 +0200

    bgu

--- report-CSI.php
+++ report-CSI.php
@@ -45,7 +45,7 @@
                 margin-top: 1px!important;
                 margin-bottom: 1px!important;
              }
-             input[type="text"]{
+             input[type="text"],input[type="date"]{
                 height: 0px!important;
              }
 
