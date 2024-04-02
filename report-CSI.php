0892639ed9d701d8fd2f37ce1cd736a4381765e7
Guillaume Blin <guillaume.blin@u-bordeaux.fr>
2024-04-02 11:21:34 +0200

    bug

--- report-CSI.php
+++ report-CSI.php
@@ -1,6 +1,10 @@
 <link href="/application/files/cache/css/ub_tiers/main.css" rel="stylesheet" type="text/css" media="all">
 
+<div>
+<h1>Rapport annuel du comité de suivi individuel de thèse</h1>
+
 <?php 
 $report_read_only=true;
 include('form-CSI.php');
 ?>
+</div>
