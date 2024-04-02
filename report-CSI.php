132b2f190f2d54002cebb684763e4baf3ddab440
Guillaume Blin <guillaume.blin@u-bordeaux.fr>
2024-04-02 16:01:57 +0200

    bug

--- report-CSI.php
+++ report-CSI.php
@@ -40,7 +40,7 @@
                 text-transform:none!important;
              }
              h5{
-                margin-bottom: 1px;
+                margin-bottom: 0px;
              }
 
         }
