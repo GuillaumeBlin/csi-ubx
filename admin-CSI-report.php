7094d26dfff4244172755c28840c9ee633894ecc
Guillaume Blin <guillaume.blin@u-bordeaux.fr>
2024-04-05 17:40:18 +0200

    bug

--- admin-CSI-report.php
+++ admin-CSI-report.php
@@ -124,6 +124,8 @@ $actionShowCSIReport = str_replace("/load_admin_CSI/", "/show_CSIReport/", $_SER
                 ids = table.rows({
                     selected: true
                 }).data();
+                if(confirm('Vous êtes sur le point de supprimer '+ids.lenght+' rapports. Etes-vous sûr.e de vouloir continuer ?')){
+
                 $.each(ids, function(index, value) {
                     var anId = value[2];
                     $.post("<?php echo $actionRemoveCSIReport; ?>", {
@@ -135,6 +137,7 @@ $actionShowCSIReport = str_replace("/load_admin_CSI/", "/show_CSIReport/", $_SER
                         selected: true
                     }).remove().draw(false);
                 });
+            }
             });
 
             table.on('click', 'tbody td.dt-control', function() {
