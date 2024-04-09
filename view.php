34394ef33122cef7bf35fdd99647c0746580aea6
Guillaume Blin <guillaume.blin@u-bordeaux.fr>
2024-04-09 17:02:18 +0200

    bug

--- view.php
+++ view.php
@@ -21,6 +21,9 @@ if ($admin == 'True') {
   <p>Les doctorantes et doctorants peuvent obtenir les liens de saisies, qu'elle ou il doit transmettre 
     aux différentes parties en accédant à l'url suivante et en remplacant <code>MONINE</code> par son INE</p>
     <p>https://doctorat.u-bordeaux.fr/page-de-saisie-des-rapports-de-csi?ine=MONINE</p>
+    ou 
+    <p>https://doctorat.u-bordeaux.fr/en/csi-report-filling-page?ine=MONINE</p>
+    pour la version en anglais
   <h3 onclick="$('#admin-links-display-<?php echo $bID; ?>').toggle();"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-right-square-fill" viewBox="0 0 16 16">
       <path d="M14 16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2zM5.904 5.197 10 9.293V6.525a.5.5 0 0 1 1 0V10.5a.5.5 0 0 1-.5.5H6.525a.5.5 0 0 1 0-1h2.768L5.197 5.904a.5.5 0 0 1 .707-.707" />
     </svg> Liens de connection</h3>
