617f9db3a60a1e68b589dffddce2408befeafbd9
Guillaume Blin <guillaume.blin@u-bordeaux.fr>
2024-04-16 13:01:46 +0200

    bug

--- controller.php
+++ controller.php
@@ -181,6 +181,7 @@ class Controller extends BlockController
         //echo $statement->rowCount(); 
         if (($statement->rowCount() > 0) && (strcmp($report_data[0]['ReadOnly'],"Oui")!=0)) {
             if (strcmp($this->langage, "FR") == 0) {
+                echo $report_data[0]['ReadOnly'];
                 echo "<b>Votre rapport a été enregistré.</b><br/>";
                 echo "Il est visible ici : <i class='far fa-file-alt'></i>";
                 echo "<script>";
