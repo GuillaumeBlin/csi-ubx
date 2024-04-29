dba98fde7c2c761cdb7b67daeef2fd6455ebdf20
Guillaume Blin <guillaume.blin@u-bordeaux.fr>
2024-04-29 12:17:25 +0200

    bug

--- view.php
+++ view.php
@@ -29,7 +29,7 @@ Le mot de passe à transmettre aux directions de thèse est <code id="admin-DT-p
     pour la version en anglais
   <h3 onclick="$('#admin-links-display-<?php echo $bID; ?>').toggle();"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-right-square-fill" viewBox="0 0 16 16">
       <path d="M14 16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2zM5.904 5.197 10 9.293V6.525a.5.5 0 0 1 1 0V10.5a.5.5 0 0 1-.5.5H6.525a.5.5 0 0 1 0-1h2.768L5.197 5.904a.5.5 0 0 1 .707-.707" />
-    </svg> Liens de connection</h3>
+    </svg> Liens de connexion</h3>
 
   <div id="admin-links-display-<?php echo $bID; ?>" style="display:none">
     <div class="d-flex align-items-center">
