ba7a7d76007fd2144d0216f107236deb83e1d487
Guillaume Blin <guillaume.blin@u-bordeaux.fr>
2024-05-28 13:14:25 +0200

    bug

--- controller.php
+++ controller.php
@@ -387,7 +387,7 @@ class Controller extends BlockController
             $user = $val[2];
             if (strcmp($user, "DT") == 0) {
                 if($_REQUEST["pp"]!=$this->pwd){
-                    echo 'Invalid request';
+                    $this->redirect('https://doctorat.u-bordeaux.fr/page-de-saisie-des-rapports-de-csi');                    
                     exit;
                 }
             }
@@ -432,7 +432,7 @@ class Controller extends BlockController
             //echo "<script>if(prompt(\"Veuillez fournir le mot de passe personnel disponible sur votre profil ADUM (haut de la page sous l'intitulé 'Pass CSI Bordeaux :') pour accéder à cette page.\")!='" . $student["passphrase"] . "'){window.location.replace('https://doctorat.u-bordeaux.fr/page-de-saisie-des-rapports-de-csi');\$('body').empty();}</script>";
             
             if($_REQUEST["pp"]!=$student["passphrase"]){
-                echo 'Invalid request';
+                $this->redirect('https://doctorat.u-bordeaux.fr/page-de-saisie-des-rapports-de-csi');
                 exit;
             }
 
