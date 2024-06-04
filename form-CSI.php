37ef055521dbd9a05acdc8185d5e9dfcfd4441ab
Guillaume Blin <guillaume.blin@u-bordeaux.fr>
2024-06-04 09:43:59 +0200

    bug

--- form-CSI.php
+++ form-CSI.php
@@ -243,7 +243,7 @@
         <label>Avis circonstancié</label>
         <textarea type="textarea" rows="5" class="form-control" name="AvisReinscription"><?php echo $report["AvisReinscription"]; ?></textarea>
     </div>
-    <?php if ($report_read_only == true) { ?>
+    <?php if ($report_read_only == false) { ?>
     <h5>Le comité de suivi alerte la direction de l’école doctorale sur « toute forme de conflit, de discrimination ou de harcèlement moral ou sexuel ou d’agissement sexiste » par un rapport confidentiel </h5>
     <h6>Cette information ne sera pas présente sur le rapport définitif mais accessible par la direction de l'ED</h6>
     <span>
