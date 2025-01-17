0a4096bd955ce4ec5b782625f563cbb8cf6aaf68
Guillaume Blin <guillaume.blin@u-bordeaux.fr>
2025-01-17 11:31:26 +0100

    bug lang

--- empty-report-PhD.php
+++ empty-report-PhD.php
@@ -1,3 +1,6 @@
+<?php
+if(!isset($lang)){$lang="FR";}
+?>
 <script type="text/javascript" src="/concrete/js/jquery.js"></script>
 <link href="/application/files/cache/css/ub_tiers/main.css" rel="stylesheet" type="text/css" media="all">
 
