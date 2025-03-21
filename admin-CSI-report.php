3a79ce2bdab16827f76a3fe2ffaf68aa4111122f
Guillaume Blin <guillaume.blin@u-bordeaux.fr>
2025-03-21 09:51:14 +0100

    bug

--- admin-CSI-report.php
+++ admin-CSI-report.php
@@ -128,6 +128,10 @@ $actionURLAllCSI = str_replace("/load_admin_CSI/", "/show_AllCSIReport/", $_SERV
                     selected: true
                 }).data();
                 console.log(ids);
+                console.log(table.rows({
+                    selected: true
+                }).ids());
+                //ids[0][2]
                 return;
                 if (ids.length>0 && confirm('Vous êtes sur le point de supprimer ' + ids.length + ' rapport(s). Etes-vous sûr(e) de vouloir continuer ?')) {
 
